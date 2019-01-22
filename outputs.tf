de0b6d2a6abe2ba4706ee85d507188b8a45a9696
Arul Kandasamy <akandasamy@virtusa.com>
2019-01-22 16:04:50 +0000

    commenting output temporarily

--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,10 @@
      value = "${google_compute_instance.gitlab-ce.network_interface.0.access_config.0.assigned_nat_ip}"
  }
 
- output "initial_root_password" {
-     value = "${data.template_file.gitlab.vars.initial_root_password}"
- }
+#  output "initial_root_password" {
+#      value = "${data.template_file.gitlab.vars.initial_root_password}"
+#  }
 
- output "runner_token" {
-     value = "${data.template_file.gitlab.vars.runner_token}"
- }
+#  output "runner_token" {
+#      value = "${data.template_file.gitlab.vars.runner_token}"
+#  }
