a2417cefbb0c071e0e8162464aa7fac40c5c6878
Arul Kandasamy <akandasamy@virtusa.com>
2019-01-22 15:58:51 +0000

    fixing service account error

--- service_account.tf
+++ service_account.tf
@@ -1,8 +1,8 @@
 resource "google_service_account" "gitlabaccount" {
-  account_id   = "gitlabaccount"
+  account_id   = "gitlab_sa"
   display_name = "Gitlab Service Account"
 }
 
 resource "google_service_account_key" "glsakey" {
-  service_account_id = "${google_service_account.myaccount.name}"
+  service_account_id = "${google_service_account.gitlabaccount.name}"
 }
