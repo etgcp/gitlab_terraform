155b1c7bb6a42525714adba9ae9b535061ad976e
arulkumarkandasamy <karul43@yahoo.co.in>
2019-01-22 23:59:58 +0000

    updating outputs

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,12 @@
      value = "${google_compute_instance.gitlab-ce.network_interface.0.access_config.0.assigned_nat_ip}"
  }
 
- output "gitlabsa_email" {
-     value =  "${google_service_account.gitlabaccount.email}"
+ output "gitlab-ce_email" {
+     value =  "${google_service_account.gitlab-ce.email}"
+ }
+
+ output "gitlab-ci-runner_email" {
+     value =  "${google_service_account.gitlab-ci-runner.email}"
  }
 
 # output "initial_root_password" {
