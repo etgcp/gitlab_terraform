447607510ade1411d5e718f12c5a9fb541ef3abe
Arul Kandasamy <akandasamy@virtusa.com>
2019-01-22 16:20:35 +0000

    commenting out to resolve permission error

--- network.tf
+++ network.tf
@@ -16,16 +16,16 @@ resource "google_compute_firewall" "external_ports_ssl" {
     }
 }
 
-resource "google_compute_firewall" "external_ports_no_ssl" {
-    count = "${var.ssl_certificate != "/dev/null" ? 0 : var.deploy_gitlab ? 1 : 0}"
-    name = "${var.prefix}${var.external_ports_name}"
-    network = "${var.network}"
+# resource "google_compute_firewall" "external_ports_no_ssl" {
+#     count = "${var.ssl_certificate != "/dev/null" ? 0 : var.deploy_gitlab ? 1 : 0}"
+#     name = "${var.prefix}${var.external_ports_name}"
+#     network = "${var.network}"
 
-    allow {
-        protocol = "tcp"
-        ports = "${var.public_ports_no_ssl}"
-    }
-}
+#     allow {
+#         protocol = "tcp"
+#         ports = "${var.public_ports_no_ssl}"
+#     }
+# }
 
 resource "google_compute_address" "external_ip" {
     count = "${var.deploy_gitlab ? 1 : 0}"
