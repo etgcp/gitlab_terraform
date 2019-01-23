51d986778bd42d6bfd80a37c859477943d82a581
Arul Kandasamy <akandasamy@virtusa.com>
2019-01-23 16:42:39 +0000

    updating backend

--- backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "terraform_bootstrap_states"
-    prefix = "gitlab"
-    credentials = "${file("${var.auth_file}")}"
-  }
-}
--- provider.tf
+++ provider.tf
@@ -3,3 +3,11 @@ provider "google" {
     project = "${var.project}"
     region = "${var.region}"
 }
+
+terraform {
+  backend "gcs" {
+    bucket = "terraform_bootstrap_states"
+    prefix = "gitlab/default.tfstate"
+    credentials = "${file("${var.auth_file}")}"
+  }
+}
