336751bf72c6ca19f2b74e3370b95671892d958d
Arul Kandasamy <akandasamy@virtusa.com>
2019-01-23 16:35:38 +0000

    updating credentials file in backend

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "gcs" {
     bucket = "terraform_bootstrap_states"
     prefix = "gitlab"
-    credentials = "londonuk-890120be22b4.json"
+    credentials = "${file("${var.auth_file}")}"
   }
 }
